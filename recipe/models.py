f7efc465e295eee18b3e6c609913be159d22eb1c
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-17 23:22:32 +0300

    create category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
