1bae1453d8152bbbebebe81cf21e43c0e7357d58
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 00:29:33 +0300

    add Recipe model to admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from recipe.models import Category
+from recipe.models import Category, Recipe
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
     search_fields = ('name',)
     ordering = ('name',)
