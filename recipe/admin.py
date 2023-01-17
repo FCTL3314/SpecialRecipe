283edf494e6eb1bbd42d544149ab0bf4ca5fe620
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 00:26:45 +0300

    add Category model to admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from recipe.models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
