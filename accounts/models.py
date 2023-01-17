989476295d60489f86dbe03ad5f2bf5718f81e5a
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-17 23:20:45 +0300

    create verbose names and str method

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.username
