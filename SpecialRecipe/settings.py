02d66927f03ea8464ac87d91af318bfab1aabc6c
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 00:21:28 +0300

    add AUTH_USER_MODEL setting

--- SpecialRecipe/settings.py
+++ SpecialRecipe/settings.py
@@ -130,3 +130,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model
+
+AUTH_USER_MODEL = 'accounts.User'
