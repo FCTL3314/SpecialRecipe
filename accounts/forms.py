fbaa86763af925bd807e5fcf07cf0b1c95e73bea
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 22:28:01 +0300

    create UserLoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from accounts.models import User
 
@@ -25,3 +25,21 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter Username or Email',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter Password',
+    }))
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={
+        'class': 'form-check-input',
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
