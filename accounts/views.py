0d8752c70aaf7982dcaadb009fca126499e5e09b
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 21:11:58 +0300

    create UserRegistrationView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from accounts.models import User
+from accounts.forms import UserRegistrationForm
+
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'accounts/registration.html'
+    success_url = reverse_lazy('recipe:recipes')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Special Recipe - Registration'
+        return context
