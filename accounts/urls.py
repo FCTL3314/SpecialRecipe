397d960a8fe6ceed1e43ae6b504591a2b7784d9a
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 22:30:49 +0300

    create UserLoginView, LogoutView urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
-from accounts.views import UserRegistrationView
+from accounts.views import UserRegistrationView, UserLoginView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
