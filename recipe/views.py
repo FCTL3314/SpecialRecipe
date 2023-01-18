01faa1b197f4b3db61b22a27b2ed6877d003e776
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 19:48:23 +0300

    create RecipesListView

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from recipe.models import Category, Recipe
+
+
+class RecipesListView(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    template_name = 'recipe/recipe.html'
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        products = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            return products.filter(category_id=category_id).order_by('name')
+        else:
+            return products.all().order_by('name')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Special Recipe - Recipes'
+        context['categories'] = Category.objects.all()
+        context['selected_category'] = self.kwargs.get('category_id')
+        return context
