43717c91ba0bb2fcc89418394f2bacc4ec1116b2
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 23:25:04 +0300

    create recipe description view url

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from recipe.views import RecipesListView
+from recipe.views import RecipesListView, DescriptionView
 
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', RecipesListView.as_view(), name='recipes'),
+    path('description/<int:category_id>/', DescriptionView.as_view(), name='description'),
     path('category/<int:category_id>/', RecipesListView.as_view(), name='category'),
     path('page/<int:page>/', RecipesListView.as_view(), name='paginator'),
     path('category/<int:category_id>/page/<int:page>/', RecipesListView.as_view(), name='category_paginator'),
