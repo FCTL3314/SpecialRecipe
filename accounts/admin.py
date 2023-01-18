4dfc9a2b257fbf6868b4dc3363d284569f7d68e4
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 11:36:27 +0300

    register User admin model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+    search_fields = ('username', 'first_name', 'last_name')
