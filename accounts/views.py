60eb85f6e7626cb0f7fa156b9eaaeee589eb8ffc
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-18 22:30:12 +0300

    create UserLoginView

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
 from accounts.models import User
-from accounts.forms import UserRegistrationForm
+from accounts.forms import UserRegistrationForm, UserLoginForm
 
 
 class UserRegistrationView(CreateView):
@@ -14,4 +15,15 @@ class UserRegistrationView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['title'] = 'Special Recipe - Registration'
+        return context
+
+
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('recipe:recipes')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Special Recipe - Login'
         return context
