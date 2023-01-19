f2f8b58b991c643100f670a3f0c71c225f0d0641
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-19 16:50:16 +0300

    add (add to saved, remove from saved) views urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from recipe.views import RecipesListView, DescriptionView
+from recipe.views import RecipesListView, DescriptionView, add_to_saved, remove_from_saved
 
 
 app_name = 'recipe'
@@ -11,4 +11,6 @@ urlpatterns = [
     path('category/<int:category_id>/', RecipesListView.as_view(), name='category'),
     path('page/<int:page>/', RecipesListView.as_view(), name='paginator'),
     path('category/<int:category_id>/page/<int:page>/', RecipesListView.as_view(), name='category_paginator'),
+    path('saves/add/<int:recipe_id>/', add_to_saved, name='add_to_saved'),
+    path('saves/remove/<int:recipe_id>/', remove_from_saved, name='remove_from_saved'),
 ]
