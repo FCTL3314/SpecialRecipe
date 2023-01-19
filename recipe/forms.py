e4db48d0c40be14506916efb479c8ded66c0e162
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-19 19:40:44 +0300

    create recipe search form

---
+++ recipe/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control me-2',
+        'placeholder': 'Enter recipe name',
+        'type': 'search',
+        'aria-label': 'Search',
+    }))
