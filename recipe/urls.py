15a85b734e419873b020e5126ed3f83436344ff4
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-20 18:11:58 +0300

    slug argument instead of id

--- recipe/urls.py
+++ recipe/urls.py
@@ -8,11 +8,11 @@ app_name = 'recipe'
 
 urlpatterns = [
     path('', RecipesListView.as_view(), name='recipes'),
-    path('category/<int:category_id>/', RecipesListView.as_view(), name='category'),
+    path('description/<slug:recipe_slug>/', DescriptionView.as_view(), name='description'),
+    path('category/<slug:category_slug>/', RecipesListView.as_view(), name='category'),
     path('page/<int:page>/', RecipesListView.as_view(), name='paginator'),
-    path('category/<int:category_id>/page/<int:page>/', RecipesListView.as_view(), name='category_paginator'),
+    path('category/<slug:category_slug>/page/<int:page>/', RecipesListView.as_view(), name='category_paginator'),
     path('saves/add/<int:recipe_id>/', add_to_saved, name='add_to_saved'),
     path('saves/remove/<int:recipe_id>/', remove_from_saved, name='remove_from_saved'),
     path('saves/', login_required(SavesListView.as_view()), name='saves'),
-    path('<slug:slug>/', DescriptionView.as_view(), name='description'),
 ]
