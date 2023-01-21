508109a2251b2d57a25a10f8727a61377b4f0b5f
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-21 14:18:08 +0300

    user logout_required decorator for register and login urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 
 from accounts.views import UserRegistrationView, UserLoginView, UserProfileView
+from accounts.decorators import logout_required
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('registration/', UserRegistrationView.as_view(), name='registration'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('registration/', logout_required(UserRegistrationView.as_view()), name='registration'),
+    path('login/', logout_required(UserLoginView.as_view()), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user/<int:pk>/', UserProfileView.as_view(), name='profile'),
 ]
