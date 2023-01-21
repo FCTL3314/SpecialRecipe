b27e40b772e99dcc27150fbc6ab3e98df28ea651
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-21 17:12:01 +0300

    custom user auth by username or email

---
+++ accounts/auth.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+
+class EmailOrUsernameAuth(ModelBackend):
+    """
+    User authentication by email or username.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(Q(email__iexact=username) | Q(username__iexact=username))
+        except user_model.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
