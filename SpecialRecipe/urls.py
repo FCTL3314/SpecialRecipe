d9ecfaa5607a49b29b41aeda8f8472fee83fc021
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-22 20:29:37 +0300

    Revert "debug toolbar url"
    This reverts commit 4b96160f0a8130d1e08fcd66a14429e123c81d9e.

--- SpecialRecipe/urls.py
+++ SpecialRecipe/urls.py
@@ -29,8 +29,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
