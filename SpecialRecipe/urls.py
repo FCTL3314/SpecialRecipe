bc4108ab848e7342bc6d3ffd8b6b658de469409e
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-22 20:33:35 +0300

    debug toolbar url fix

--- SpecialRecipe/urls.py
+++ SpecialRecipe/urls.py
@@ -29,4 +29,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
