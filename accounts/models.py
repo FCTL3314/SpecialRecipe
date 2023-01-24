1c5317890f755a62cb90d3aab890de8130305ebe
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-24 21:11:38 +0300

    convert email to lowercase to exclude registration with the same emails that differ only in case

--- accounts/models.py
+++ accounts/models.py
@@ -6,9 +6,13 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', null=True, blank=True)
     email = models.EmailField(db_index=True, unique=True, max_length=254)
 
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
+    def clean(self):
+        self.email = self.email.lower()
+        return super().clean()
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
