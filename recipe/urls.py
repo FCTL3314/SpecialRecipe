b11b96bc9a7f3122787570d2bc018327d2677278
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-24 13:53:00 +0300

    rename url names remove_from_saved -> remove-from-saved, add_to_saved -> add-to-saved, category_paginator -> category-paginator

--- recipe/urls.py
+++ recipe/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('description/<slug:recipe_slug>/', DescriptionView.as_view(), name='description'),
     path('category/<slug:category_slug>/', RecipesListView.as_view(), name='category'),
     path('page/<int:page>/', RecipesListView.as_view(), name='paginator'),
-    path('category/<slug:category_slug>/page/<int:page>/', RecipesListView.as_view(), name='category_paginator'),
-    path('saves/add/<int:recipe_id>/', add_to_saved, name='add_to_saved'),
-    path('saves/remove/<int:recipe_id>/', remove_from_saved, name='remove_from_saved'),
+    path('category/<slug:category_slug>/page/<int:page>/', RecipesListView.as_view(), name='category-paginator'),
+    path('saves/add/<int:recipe_id>/', add_to_saved, name='add-to-saved'),
+    path('saves/remove/<int:recipe_id>/', remove_from_saved, name='remove-from-saved'),
     path('saves/', login_required(SavesListView.as_view()), name='saves'),
 ]
