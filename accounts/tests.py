9b835e7159cc6b702edfca97d3d3f8e8371c978a
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-26 23:14:54 +0300

    tests for UserLoginView

--- accounts/tests.py
+++ accounts/tests.py
@@ -79,3 +79,99 @@ class UserRegistrationViewTestCase(TestCase):
         self.assertContains(response, 'This password is too short. It must contain at least 8 characters.')
         self.assertContains(response, 'This password is too common.')
         self.assertContains(response, 'This password is entirely numeric.')
+
+
+class UserLoginViewTestCase(TestCase):
+
+    def setUp(self):
+        self.path = reverse('accounts:login')
+        self.data = {
+            'username': 'TestUser',
+            'password': 'qnjCmk27yzKTCWWiwdYH',
+        }
+        User.objects.create_user(
+            username=self.data['username'],
+            email='testuser@mail.com',
+            password=self.data['password']
+        )
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Special Recipe | Login')
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_user_login_get(self):
+        response = self.client.get(self.path)
+
+        self._common_tests(response)
+
+    def test_user_login_post_with_username(self):
+        username = self.data['username']
+
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_login_post_via_email(self):
+        email = 'testuser@mail.com'
+        email_data = self.data.copy()
+        email_data['username'] = email
+
+        self.assertTrue(User.objects.filter(email=email).exists())
+
+        response = self.client.post(self.path, email_data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_login_post_with_remember_be(self):
+        username = self.data['username']
+
+        data_with_remember_me = self.data.copy()
+        data_with_remember_me['remember_me'] = 'on'
+
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        response = self.client.post(self.path, data_with_remember_me)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(response.cookies['sessionid']['max-age'], 1209600)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_login_post_without_remember_me(self):
+        username = self.data['username']
+
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(response.cookies['sessionid']['max-age'], 1800)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_login_post_invalid_username(self):
+        username = 'InvalidUsername'
+        invalid_username_data = self.data.copy()
+        invalid_username_data['username'] = username
+
+        self.assertFalse(User.objects.filter(username=username).exists())
+
+        response = self.client.post(self.path, invalid_username_data)
+
+        self._common_tests(response)
+        self.assertContains(response, 'Invalid username/email or password.')
+
+    def test_user_login_post_invalid_password(self):
+        password = 'InvalidPassword'
+        invalid_password_data = self.data.copy()
+        invalid_password_data['password'] = password
+
+        self.assertTrue(User.objects.filter(username=invalid_password_data['username']).exists())
+
+        response = self.client.post(self.path, invalid_password_data)
+
+        self._common_tests(response)
+        self.assertContains(response, 'Invalid username/email or password.')
