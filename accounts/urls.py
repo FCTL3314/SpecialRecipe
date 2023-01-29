a02a5e45e6995bfa6cd3b874b149ffbe4a44095f
F_C_T_L <solovev.nikita.05@gmail.com>
2023-01-29 14:00:52 +0300

    urls for SendVerificationEmail, EmailVerification views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from accounts.decorators import logout_required
-from accounts.views import UserLoginView, UserProfileView, UserRegistrationView
+from accounts.views import (EmailVerificationView, SendVerificationEmailView,
+                            UserLoginView, UserProfileView,
+                            UserRegistrationView)
 
 app_name = 'accounts'
 
@@ -11,4 +14,10 @@ urlpatterns = [
     path('login/', logout_required(UserLoginView.as_view()), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user/<int:pk>/', UserProfileView.as_view(), name='profile'),
+    path(
+        'verification/send/<str:email>/',
+        login_required(SendVerificationEmailView.as_view()),
+        name='send-verification-email'
+    ),
+    path('verify/<str:email>/<uuid:code>/', login_required(EmailVerificationView.as_view()), name='email-verification'),
 ]
