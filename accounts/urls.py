1d3f5d1e5d2816ec36df714c39bbc0c09bd8cefb
F_C_T_L <solovev.nikita.05@gmail.com>
2023-02-06 18:33:06 +0300

    simplification

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,20 +17,16 @@ urlpatterns = [
     path('login/', logout_required(UserLoginView.as_view()), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user/<slug:slug>/', UserProfileView.as_view(), name='profile'),
-    path(
-        'verification/send/<str:email>/',
-        login_required(SendVerificationEmailView.as_view()),
-        name='send-verification-email'
-    ),
+
+    path('verification/send/<str:email>/', login_required(SendVerificationEmailView.as_view()),
+         name='send-verification-email'),
     path('verify/<str:email>/<uuid:code>/', login_required(EmailVerificationView.as_view()), name='email-verification'),
+
     path('password_change/', PwdChangeView.as_view(), name='password_change'),
     path('password_change/done/', PwdChangeDoneView.as_view(), name='password_change_done'),
+
     path('password_reset/', logout_required(PwdResetView.as_view()), name='reset_password'),
     path('password_reset/done/', logout_required(PwdResetDoneView.as_view()), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', logout_required(PwdResetConfirmView.as_view()), name='password_reset_confirm'),
-    path(
-        'reset/done/',
-        logout_required(PwdResetCompleteView.as_view(template_name='accounts/password/password_reset_complete.html')),
-        name='password_reset_complete'
-    ),
+    path('reset/done/', logout_required(PwdResetCompleteView.as_view()), name='password_reset_complete'),
 ]
