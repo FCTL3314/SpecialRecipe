242d0cd30336ad121ebddcc2979d93eec74f43f5
F_C_T_L <solovev.nikita.05@gmail.com>
2023-02-07 15:35:12 +0300

    remove default django auth views, use own instead

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,30 +3,24 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from accounts.decorators import logout_required
-from accounts.views import (EmailVerificationView, PwdChangeDoneView,
-                            PwdChangeView, PwdResetCompleteView,
-                            PwdResetConfirmView, PwdResetDoneView,
-                            PwdResetView, SendVerificationEmailView,
-                            UserLoginView, UserProfileView,
-                            UserRegistrationView)
+from accounts import views as account_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('registration/', logout_required(UserRegistrationView.as_view()), name='registration'),
-    path('login/', logout_required(UserLoginView.as_view()), name='login'),
+    path('registration/', logout_required(account_view.UserRegistrationView.as_view()), name='registration'),
+    path('login/', logout_required(account_view.UserLoginView.as_view()), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('user/<slug:slug>/', UserProfileView.as_view(), name='profile'),
 
-    path('verification/send/<str:email>/', login_required(SendVerificationEmailView.as_view()),
+    path('user/<slug:slug>/', account_view.UserProfileView.as_view(), name='profile'),
+    path('user/password/<slug:slug>/', account_view.UserProfilePasswordView.as_view(), name='profile-password'),
+
+    path('verification/send/<str:email>/', login_required(account_view.SendVerificationEmailView.as_view()),
          name='send-verification-email'),
-    path('verify/<str:email>/<uuid:code>/', login_required(EmailVerificationView.as_view()), name='email-verification'),
+    path('verify/<str:email>/<uuid:code>/', login_required(account_view.EmailVerificationView.as_view()),
+         name='email-verification'),
 
-    path('password_change/', PwdChangeView.as_view(), name='password_change'),
-    path('password_change/done/', PwdChangeDoneView.as_view(), name='password_change_done'),
-
-    path('password_reset/', logout_required(PwdResetView.as_view()), name='reset_password'),
-    path('password_reset/done/', logout_required(PwdResetDoneView.as_view()), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', logout_required(PwdResetConfirmView.as_view()), name='password_reset_confirm'),
-    path('reset/done/', logout_required(PwdResetCompleteView.as_view()), name='password_reset_complete'),
+    path('password_reset/', logout_required(account_view.PwdResetView.as_view()), name='reset_password'),
+    path('reset/<uidb64>/<token>/', logout_required(account_view.PwdResetConfirmView.as_view()),
+         name='password_reset_confirm'),
 ]
