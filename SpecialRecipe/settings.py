b4511c7d89a51e28f5dd84811f21b09f7d69ec5f
F_C_T_L <solovev.nikita.05@gmail.com>
2023-02-07 15:24:29 +0300

    context processor for current url name

--- SpecialRecipe/settings.py
+++ SpecialRecipe/settings.py
@@ -95,6 +95,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'accounts.context_processors.current_url_name',
             ],
         },
     },
