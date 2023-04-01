c933fd53febaa5040abb176e016ddc5b4fb0f2b3
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-04-01 17:46:05 +0300

    Rewrote js functions from ajax to fetch

--- static/js/script.js
+++ static/js/script.js
@@ -17,102 +17,215 @@ function getCookie(name) {
 }
 
 
-$('.bookmark').click(function () {
-    const recipeId = $(this).data('recipe-id');
-    const bookmarkLink = $(this);
-    const bookmarkImg = bookmarkLink.find('img');
-    const isSaved = bookmarkLink.attr('data-is-saved') === 'true';
+const bookmarkLinks = document.querySelectorAll('.bookmark')
 
-    $.ajax({
-        type: isSaved ? 'DELETE' : 'POST',
-        url: isSaved ? '/api/v1/bookmarks/remove/' : '/api/v1/bookmarks/add/',
-        data: {'recipe_id': recipeId},
-        headers: {'X-CSRFToken': getCookie('csrftoken')},
-        success: function () {
-            const bookmarkText = bookmarkLink.find('span');
-            const bookmarkCount = parseInt(bookmarkText.text());
+if (bookmarkLinks) {
+    bookmarkLinks.forEach(bookmarkLink => {
+        bookmarkLink.addEventListener('click', function () {
+            const recipeId = this.dataset.recipeId;
+            const bookmarkImg = bookmarkLink.querySelector('img');
+            const isSaved = bookmarkLink.dataset.isSaved === 'true';
+            const bookmarkText = bookmarkLink.querySelector('span');
+            const bookmarkCount = parseInt(bookmarkText.textContent);
             const newBookmarkCount = isSaved ? bookmarkCount - 1 : bookmarkCount + 1;
 
-            bookmarkText.text(newBookmarkCount + ' Saves');
-            bookmarkImg.attr('src', `${static_url}icon/bookmark${isSaved ? '' : '-fill'}.svg`);
-            bookmarkImg.addClass('animate__animated animate__flip').one('animationend', function () {
-                $(this).removeClass('animate__animated animate__flip');
-            });
-            bookmarkLink.attr('data-is-saved', !isSaved);
-        },
-        error: function (xhr, status, error) {
-            if (xhr.status === 403) {
-                const currentUrl = window.location.pathname;
-                const loginUrl = '/accounts/login/?next=' + currentUrl;
-                window.location.replace(loginUrl);
-            } else {
-                console.error(error);
-            }
-        }
+            const url = isSaved ? '/api/v1/bookmarks/remove/' : '/api/v1/bookmarks/add/';
+            const method = isSaved ? 'DELETE' : 'POST';
+            const csrfToken = getCookie('csrftoken');
+            const headers = {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken,
+            };
+            const body = JSON.stringify({'recipe_id': recipeId});
+
+            fetch(url, {method, headers, body})
+                .then(response => {
+                    if (response.status === 204) {
+                        return response
+                    } else if (response.ok) {
+                        return response.json();
+                    } else if (response.status === 403) {
+                        const error = new Error(response.statusText);
+                        error.status = 403;
+                        throw error;
+                    } else {
+                        const error = new Error(response.statusText);
+                        error.status = response.status
+                        throw error;
+                    }
+                })
+                .then(() => {
+                    bookmarkText.textContent = `${newBookmarkCount} Saves`;
+                    bookmarkImg.src = `${static_url}icon/bookmark${isSaved ? '' : '-fill'}.svg`;
+                    bookmarkImg.classList.add('animate__animated', 'animate__flip');
+                    bookmarkImg.addEventListener('animationend', function () {
+                        this.classList.remove('animate__animated', 'animate__flip');
+                    });
+                    bookmarkLink.dataset.isSaved = (!isSaved).toString();
+                })
+                .catch(error => {
+                    if (error.status === 403) {
+                        const currentUrl = window.location.pathname;
+                        const loginUrl = '/accounts/login/?next=' + currentUrl;
+                        window.location.replace(loginUrl);
+                    } else {
+                        console.error(error.message);
+                    }
+                });
+        });
     });
-});
+}
 
-$('#show-more-categories-btn').click(function () {
-    const showMoreButton = $(this);
-    const page = parseInt(showMoreButton.attr('data-page'));
-    const selectedCategorySlug = showMoreButton.attr('data-selected-category-slug');
+const showMoreCategoriesButton = document.querySelector('#show-more-categories-btn')
 
-    $.ajax({
-        type: 'GET',
-        url: '/api/v1/categories/',
-        data: {
-            page: page,
-        },
-        success: function (response) {
-            for (const category of response.results) {
+if (showMoreCategoriesButton) {
+    showMoreCategoriesButton.addEventListener('click', function () {
+        const page = parseInt(showMoreCategoriesButton.dataset.page);
+        const selectedCategorySlug = showMoreCategoriesButton.dataset.selectedCategorySlug;
 
-                const categoryName = category.name;
-                const categorySlug = category.slug;
+        const url = `/api/v1/categories/?page=${page}`;
+        const method = 'GET';
+        const headers = {'Content-Type': 'application/json'};
 
-                const categoryItem = $(
-                    `<a class="list-group-item list-group-item-action" href="/recipes/category/${categorySlug}/">${
-                        categoryName}</a>`
-                );
-
-                if (categorySlug === selectedCategorySlug) {
-                    categoryItem.addClass('text-dark bg-body-secondary disabled');
+        fetch(url, {method, headers})
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    const error = new Error(response.statusText);
+                    error.status = response.status
+                    throw error;
                 }
-                categoryItem.insertBefore(showMoreButton);
-            }
+            })
+            .then(response => {
+                for (const category of response.results) {
 
-            response.next === null
-                ? showMoreButton.remove()
-                : showMoreButton.attr('data-page', page + 1);
-        },
-        error: function (xhr, status, error) {
-            console.error(error);
-        }
+                    const categoryName = category.name;
+                    const categorySlug = category.slug;
+
+                    const categoryItem = $(
+                        `<a class="list-group-item list-group-item-action" href="/recipes/category/${categorySlug}/">
+                           ${categoryName}
+                         </a>`
+                    );
+
+                    if (categorySlug === selectedCategorySlug) {
+                        categoryItem.addClass('text-dark bg-body-secondary disabled');
+                    }
+                    categoryItem.insertBefore(showMoreCategoriesButton);
+                }
+                response.next === null
+                    ? showMoreCategoriesButton.remove()
+                    : showMoreCategoriesButton.dataset.page = (page + 1).toString();
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
     });
-});
+}
 
-$('#show-more-comments-btn').click(function () {
-    const commentsContainer = $('#comments-container');
-    const showMoreButton = $(this);
-    const page = parseInt(showMoreButton.attr('data-page'));
-    const recipeId = parseInt(showMoreButton.attr('data-recipe-id'));
+const showMoreCommentsButton = document.querySelector('#show-more-comments-btn')
 
-    $.ajax({
-        type: 'GET',
-        url: '/api/v1/comments/',
-        data: {
-            page: page,
-            recipe_id: recipeId,
-        },
-        success: function (response) {
-            for (const comment of response.results) {
+if (showMoreCommentsButton) {
+    showMoreCommentsButton.addEventListener('click', function () {
+        const commentsContainer = document.getElementById('comments-container');
+        const page = parseInt(showMoreCommentsButton.dataset.page);
+        const recipeId = parseInt(showMoreCommentsButton.dataset.recipeId)
 
-                const authorImage = comment.author.image
-                    ? comment.author.image
+        const url = `/api/v1/comments/?page=${page}&recipe_id=${recipeId}`;
+        const method = 'GET';
+        const headers = {'Content-Type': 'application/json'};
+
+        fetch(url, {method, headers})
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    const error = new Error(response.statusText);
+                    error.status = response.status
+                    throw error;
+                }
+            })
+            .then(response => {
+                for (const comment of response.results) {
+
+                    const authorImage = comment.author.image
+                        ? comment.author.image
+                        : `${static_url}img/default_user_image.png`;
+                    const authorUsername = comment.author.username;
+                    const authorSlug = comment.author.slug;
+                    const commentCreatedDate = moment(comment.created_date).fromNow();
+                    const commentText = comment.text;
+
+                    const commentItem = $(
+                        `<div class="d-flex align-items-start mb-4">
+                           <a href="/accounts/user/${authorSlug}/">
+                             <img class="me-3 rounded-circle" src="${authorImage}" alt="user-image" width="40" 
+                                  height="40">
+                           </a>
+                           <div>
+                             <a class="fs-5 me-1 link link-dark text-decoration-none"
+                                href="/accounts/user/${authorSlug}/">${authorUsername}</a>
+                             <span class="text-body-secondary">${commentCreatedDate}</span>
+                             <p class="text-break">${commentText}</p>
+                           </div>
+                         </div>`
+                    );
+                    commentItem.appendTo(commentsContainer);
+                }
+
+                response.next === null
+                    ? showMoreCommentsButton.closest('div').remove()
+                    : showMoreCommentsButton.dataset.page = (page + 1).toString();
+
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    });
+}
+
+const addCommentForm = document.querySelector('#add-comment-form')
+
+if (addCommentForm) {
+    addCommentForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const commentContainer = document.getElementById('comments-container');
+        const noCommentsDiv = document.getElementById('no-comments');
+        const form = event.target;
+        const formData = new FormData(form);
+        const recipeId = addCommentForm.dataset.recipeId;
+        const comment = formData.get('comment');
+
+        const url = '/api/v1/comments/';
+        const method = 'POST';
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken'),
+        };
+        const body = JSON.stringify({'recipe_id': recipeId, 'text': comment})
+
+        fetch(url, {method, headers, body})
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    const error = new Error(response.statusText);
+                    error.status = response.status
+                    throw error;
+                }
+            })
+            .then(response => {
+                form.reset();
+
+                const authorImage = response.author.image
+                    ? response.author.image
                     : `${static_url}/img/default_user_image.png`;
-                const authorUsername = comment.author.username;
-                const authorSlug = comment.author.slug;
-                const commentCreatedDate = moment(comment.created_date).fromNow();
-                const commentText = comment.text;
+                const authorUsername = response.author.username;
+                const authorSlug = response.author.slug;
+                const commentCreatedDate = moment(response.created_date).fromNow();
+                const commentText = response.text;
 
                 const commentItem = $(
                     `<div class="d-flex align-items-start mb-4">
@@ -127,73 +240,15 @@ $('#show-more-comments-btn').click(function () {
                       </div>
                   </div>`
                 );
-                commentItem.appendTo(commentsContainer);
-            }
 
-            response.next === null
-                ? showMoreButton.closest('div').remove()
-                : showMoreButton.attr('data-page', page + 1);
+                if (noCommentsDiv) {
+                    noCommentsDiv.remove();
+                }
 
-        },
-        error: function (xhr, status, error) {
-            console.error(error);
-        }
+                commentItem.prependTo(commentContainer);
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
     });
-});
-
-
-$(`#add-comment-form`).on('submit', function (event) {
-    event.preventDefault();
-
-    const commentContainer = document.getElementById('comments-container');
-    const noCommentsDiv = document.getElementById('no-comments');
-    const form = event.target;
-    const formData = new FormData(form);
-    const recipeId = $(this).attr('data-recipe-id');
-    const comment = formData.get('comment');
-
-    $.ajax({
-        type: 'POST',
-        url: '/api/v1/comments/',
-        data: {
-            'recipe_id': recipeId,
-            'text': comment,
-            'X-CSRFToken': getCookie('csrftoken'),
-        },
-        headers: {'X-CSRFToken': getCookie('csrftoken')},
-        success: function (response) {
-            form.reset();
-
-            const authorImage = response.author.image
-                ? response.author.image
-                : `${static_url}/img/default_user_image.png`;
-            const authorUsername = response.author.username;
-            const authorSlug = response.author.slug;
-            const commentCreatedDate = moment(response.created_date).fromNow();
-            const commentText = response.text;
-
-            const commentItem = $(
-                `<div class="d-flex align-items-start mb-4">
-                      <a href="/accounts/user/${authorSlug}/">
-                        <img class="me-3 rounded-circle" src="${authorImage}" alt="user-image" width="40" height="40">
-                      </a>
-                      <div>
-                        <a class="fs-5 me-1 link link-dark text-decoration-none" 
-                           href="/accounts/user/${authorSlug}/">${authorUsername}</a>
-                        <span class="text-body-secondary">${commentCreatedDate}</span>
-                        <p class="text-break">${commentText}</p>
-                      </div>
-                  </div>`
-            );
-
-            if (noCommentsDiv) {
-                noCommentsDiv.remove();
-            }
-
-            commentItem.prependTo(commentContainer);
-        },
-        error: function (xhr, status, error) {
-            console.error(error);
-        }
-    });
-});
+}
