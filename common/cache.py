7f557d3da8cf4ecaaed84abb4b0d7cfe4ea26613
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-04-21 15:30:01 +0300

    Changes in template styles, improved readability.

--- common/cache.py
+++ common/cache.py
@@ -1,8 +1,7 @@
 from django.core.cache import cache
-from django.db.models import QuerySet
 
 
-def get_cached_data_or_set_new(key: str, default: callable, timeout: int) -> QuerySet:
+def get_cached_data_or_set_new(key: str, default: callable, timeout: int):
     """
     Checks if the cache exists for the given key. If not present,
     it caches the data obtained from calling the default function for
